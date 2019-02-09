5c19126ab5c4752539fb7897eada3fab28f15eca
jishan singh <singhjishan2@gmail.com>
2019-02-08 23:12:24 -0500

    empty page

--- basic/todo/models.py
+++ basic/todo/models.py
@@ -2,7 +2,6 @@ from django.db import models
 from django.urls import reverse
 # Create your models here.
 class todo_list(models.Model):
-	work=models.CharField(max_length=25)
 	image=models.FileField()
 	name=models.CharField(max_length=30)
 	def get_absolute_url(self):
